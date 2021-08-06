50503c0a8bf71a0c8c89868842077c430a4ee87e
Esa Varemo <git@esav.fi>
2021-08-06 17:08:04 +0300

    Fetch more information and create new interfaces as necessary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pdb import set_trace as st
 import pynetbox
 import subprocess
+import sys
 from typing import List
 
 URL = 'http://localhost:8000'
@@ -8,15 +9,14 @@ TOKEN = subprocess.check_output(['secret-tool', 'lookup', 'service', 'netbox']).
 
 
 TAG_COREVPN = 'Core VPN'
+TAG_PUBLIC_INTERFACE = 'Public interface'
 
 nb = pynetbox.api(URL, token=TOKEN)
 
 
-class Device:
-    def __init__(self, name: str, asn: int, interfaces: List):
-        self.name = name
-        self.asn = asn
-        self.interfaces = interfaces
+def die(message: str):
+    print(message)
+    sys.exit(1)
 
 
 def has_tag(record, required_tag):
@@ -24,6 +24,14 @@ def has_tag(record, required_tag):
         if str(tag) == required_tag:
             return True
     return False
+
+
+def find_description(records, required_description):
+    for record in records:
+        if str(record.description) == required_description:
+            return record
+    else:
+        return None
 
 
 interfaces = {}
@@ -36,7 +44,7 @@ def get_interfaces():
         api = nb.virtualization.interfaces
 
         for interface in api.all():
-            vm = interface.virtual_machine
+            vm = str(interface.virtual_machine.name)
             if vm not in interfaces.keys():
                 interfaces[vm] = [interface]
             else:
@@ -47,22 +55,69 @@ def get_interfaces():
 
 def get_interfaces_for_vm(name: str):
     interfaces = get_interfaces()
-    return interfaces[name]
+    return interfaces.get(name, [])
+
+
+def find_public_interface(interfaces: List):
+    for interface in interfaces:
+        if has_tag(interface, TAG_PUBLIC_INTERFACE):
+            return interface
+    return None
+
+
+def get_interface_ip(interface):
+    api = getattr(nb.ipam, 'ip-addresses')
+    return api.get(vminterface_id=interface.id).address.split('/')[0]
+
+
+def create_nic(vm, target_name: str):
+    existing = get_interfaces_for_vm(vm.name)
+    index = 0
+    for interface in existing:
+        if str(interface.name).startswith('wg'):
+            existing_index = int(interface.name[2:])
+            if existing_index <= index:
+                index = existing_index + 1
+
+    api = nb.virtualization.interfaces
+    return api.create({
+        "name": f"wg{index}",
+        "virtual_machine": {"name": vm.name},
+        "description": str(target_name)
+    })
 
 
 def get_devices():
     # Physical devices not implemented yet
     api = getattr(nb.virtualization, 'virtual-machines')
-    vms = []
+    vms = {}
 
     for vm in api.all():
         if not has_tag(vm, TAG_COREVPN):
             continue
         interfaces = get_interfaces_for_vm(vm.name)
-        vms.append(Device(vm.name, vm.site.asn, interfaces))
+        public_interface = find_public_interface(interfaces) or die(f"Could not find public interface for {vm.name}")
+        public_ip = get_interface_ip(public_interface)
+        setattr(vm, 'interfaces', interfaces)
+        setattr(vm, 'pubkey', vm.custom_fields['wg-public-key'])
+        setattr(vm, 'pubip', str(public_ip))
+        vms[vm.name] = vm
 
     return vms
 
 
-devices = get_devices()
-st()
+def main():
+    devices = get_devices()
+
+    device1 = devices.get(sys.argv[1], None) or die(f"Device {sys.argv[1]} not found")
+    device2 = devices.get(sys.argv[2], None) or die(f"Device {sys.argv[2]} not found")
+
+    print(device1.name, device1.pubkey, device1.pubip, device1.site.asn)
+    print(device2.name, device2.pubkey, device2.pubip, device2.site.asn)
+
+    device1_nic = find_description(device1.interfaces, device2.name) or create_nic(device1, device2.name)
+    device2_nic = find_description(device2.interfaces, device1.name) or create_nic(device2, device1.name)
+
+
+if __name__ == '__main__':
+    main()
